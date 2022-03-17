4ce3bb202e1b8d3df0b01371ec175a23103593f5
ducphamle2 <ducphamle212@gmail.com>
2022-03-17 16:39:19 +0700

    update ts file

--- packages/oraiwasm-js/src/index.d.ts
+++ packages/oraiwasm-js/src/index.d.ts
@@ -9,6 +9,8 @@ declare class OraiwasmJs extends Cosmos {
   constructor(url: any, chainId: any);
   execute(params: { childKey: any, rawInputs: RawInput[], fees?: number | any[], gasLimits?: number | string, memo?: string | undefined, gasMultiplier?: number, timeoutHeight?: number, timeoutIntervalCheck?: number, broadcastMode?: string }): Promise<any>;
   getTxBody(messages: any[], timeout_height: number | undefined, memo: string | undefined): any;
+  getHandleMessage(contract: string, msg: any, sender: string, sentFunds: any[]): any;
+  getHandleMessageSimulate(contract: string, msg: any, sender: string, sentFunds: any[]): any;
 }
 
 declare namespace OraiwasmJs { }
